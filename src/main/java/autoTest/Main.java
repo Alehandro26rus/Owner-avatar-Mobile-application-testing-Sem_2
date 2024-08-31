d4f5d2e536f0bac1e5b54e03b10e8c6d952907aa
Alehandro_26rus <skash96@yandex.ru>
2024-08-31 22:33:21 +0300

    Added other changes

--- src/main/java/autoTest/Main.java
+++ src/main/java/autoTest/Main.java
@@ -11,9 +11,9 @@ public class Main {
         container1.addComponent(product2);
         container1.display();
         container1.countPrice();
-        System.out.println("Sum " + container1.getTitle() + ": "+ container1.countPrice() + " rub.");
+        System.out.println("Sum " + container1.getTitle() + ": " + container1.countPrice() + " rub.");
         System.out.println("------------------------");
-        
+
         ProductComponent product3 = new ProductComponent("Cheese", 111);
         Container container2 = new Container("Purchase2");
         container2.addComponent(product3);
